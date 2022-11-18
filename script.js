a68416691912818845fd81fab5361de816adf62f
DyusmikeevEA <rfeugenerf@gmail.com>
2022-11-18 23:03:43 +0500

    telephone3

---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.querySelector('.menuImg')
+let menu = document.querySelector('.none')
+let header = document.querySelector('header')
+
+
+button.addEventListener('click', ()=>{
+    menu.classList.toggle('menu_content')
+    console.log(1)
+})
